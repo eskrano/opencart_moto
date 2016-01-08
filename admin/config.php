721d63bd71648de893a9830aa00d08a3255bf7c0
alex <alex.priadko2014@yandex.ru>
2016-01-08 13:47:42 +0200

    Opencart with install

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eat.dev/admin/');
+define('HTTP_CATALOG', 'http://eat.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eat.dev/admin/');
+define('HTTPS_CATALOG', 'http://eat.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/work/openservser/OpenServer/domains/eat.dev/admin/');
+define('DIR_SYSTEM', 'D:/work/openservser/OpenServer/domains/eat.dev/system/');
+define('DIR_LANGUAGE', 'D:/work/openservser/OpenServer/domains/eat.dev/admin/language/');
+define('DIR_TEMPLATE', 'D:/work/openservser/OpenServer/domains/eat.dev/admin/view/template/');
+define('DIR_CONFIG', 'D:/work/openservser/OpenServer/domains/eat.dev/system/config/');
+define('DIR_IMAGE', 'D:/work/openservser/OpenServer/domains/eat.dev/image/');
+define('DIR_CACHE', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/download/');
+define('DIR_LOGS', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/upload/');
+define('DIR_CATALOG', 'D:/work/openservser/OpenServer/domains/eat.dev/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'element');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eat.dev/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eat.dev/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/work/openservser/OpenServer/domains/eat.dev/catalog/');
+define('DIR_SYSTEM', 'D:/work/openservser/OpenServer/domains/eat.dev/system/');
+define('DIR_LANGUAGE', 'D:/work/openservser/OpenServer/domains/eat.dev/catalog/language/');
+define('DIR_TEMPLATE', 'D:/work/openservser/OpenServer/domains/eat.dev/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/work/openservser/OpenServer/domains/eat.dev/system/config/');
+define('DIR_IMAGE', 'D:/work/openservser/OpenServer/domains/eat.dev/image/');
+define('DIR_CACHE', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/download/');
+define('DIR_LOGS', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/work/openservser/OpenServer/domains/eat.dev/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'element');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
